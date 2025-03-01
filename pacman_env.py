9ec93c4ef775628e76ec834aec43e2d1e208d53e
Maria Balderas <mriabalderas0@gmail.com>
2025-02-28 17:00:51 -0800

    first try
    qwdqwd
    qcdqd2c2v

---
+++ pacman_env.py
@@ -0,0 +1,28 @@
+import gymnasium as gym
+import numpy as np
+import matplotlib.pyplot as plt
+
+from ale_py import ALEInterface
+ale = ALEInterface()
+
+# Create the environment
+env = gym.make("ALE/Pacman-v5") # Use "human" for live rendering
+
+# Reset environment
+obs, info = env.reset()
+
+# Run the environment for a few frames
+for _ in range(1000):
+    action = env.action_space.sample()  # Take a random action
+    obs, reward, terminated, truncated, info = env.step(action)  # Perform action
+
+    # Display the game screen
+    plt.imshow(obs)
+    plt.axis("off")
+    plt.title(f"Reward: {reward}")
+    plt.show()
+
+    if terminated or truncated:
+        obs, info = env.reset()
+
+env.close()
